7b404e244ee493cebc43ec7c542f8d4750758ea5
NVK <Dragontailonfire@users.noreply.github.com>
2025-08-04 17:26:45 +0000

    add basic site

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+let myButton = document.querySelector("button");
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "hello there!"
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem("name", myName);
+        myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `This is a simple website, ${storedName}`;
+}
+
+myButton.addEventListener("click", () => {
+    setUserName();
+});
